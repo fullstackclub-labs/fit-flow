89339ecf978a93f41e857bb9eecc3a3392468bb6
Paulo Santana <paulosantana.sa@gmail.com>
2023-09-10 17:09:02 -0300

    Remove `docker-entrypoint` and adjust environments.

--- docker/entrypoint-initdb.d/initialize-databse.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE fitflow_test;
-    GRANT ALL PRIVILEGES ON DATABASE fitflow_test TO fitflow;
-EOSQL
